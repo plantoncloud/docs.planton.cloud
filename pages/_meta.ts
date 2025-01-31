4dee0382e1c5bba02b6e1588f20f1a029ff7d075
sureshattaluri <suresh.attaluri789@gmail.com>
2025-01-31 12:39:55 +0530

    add secure connection sharing page

--- pages/_meta.ts
+++ pages/_meta.ts
@@ -2,6 +2,7 @@ export default {
     index: 'Welcome',
     'pp-vs-pc': 'Project Planton',
     'connections': '',
+    'secure-connection-sharing': '',
     'deployment-store': 'Deployment Store',
     'pulumi-registry': 'Pulumi Registry',
     'iac-ci-cd': 'IaC CI/CD',
@@ -10,4 +11,5 @@ export default {
     'audit': 'Audit',
     'microservices': 'Microservices',
     'k8s-d2-ops': 'Kubernetes Ops',
+
 }
